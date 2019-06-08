a8e6eabdfb659d4af67533c9545be4858869c736
Clloud <chenshaofengsf@163.com>
2019-06-08 13:46:54 +0800

    setting

--- application/common.php
+++ application/common.php
@@ -1,2 +1,136 @@
 <?php
 // 应用公共文件
+/**
+ * @param string $url get请求地址
+ * @param int $httpCode 返回状态码
+ * @return mixed
+ */
+function curl_get($url, &$httpCode = 0)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+    //不做证书校验,部署在linux环境下请改为true
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+    $file_contents = curl_exec($ch);
+    $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+    return $file_contents;
+}
+
+/**
+ * @param string $url post请求地址
+ * @param array $params
+ * @return mixed
+ */
+function curl_post($url, array $params = array())
+{
+    $data_string = json_encode($params);
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+    curl_setopt(
+        $ch, CURLOPT_HTTPHEADER,
+        array(
+            'Content-Type: application/json'
+        )
+    );
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return ($data);
+}
+
+function curl_post_raw($url, $rawData)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $rawData);
+    curl_setopt(
+        $ch, CURLOPT_HTTPHEADER,
+        array(
+            'Content-Type: text'
+        )
+    );
+    $data = curl_exec($ch);
+    curl_close($ch);
+    return ($data);
+}
+
+
+function getRandChar($length)
+{
+    $str = null;
+    $strPol = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    $max = strlen($strPol) - 1;
+
+    for ($i = 0;
+         $i < $length;
+         $i++) {
+        $str .= $strPol[rand(0, $max)];
+    }
+
+    return $str;
+}
+
+//将内容进行UNICODE编码，编码后的内容格式：\u56fe\u7247 （原始：图片）
+function unicode_encode($name)
+{
+    $name = iconv('UTF-8', 'UCS-2', $name);
+    $len = strlen($name);
+    $str = '';
+    for ($i = 0; $i < $len - 1; $i = $i + 2)
+    {
+        $c = $name[$i];
+        $c2 = $name[$i + 1];
+        if (ord($c) > 0)
+        {    // 两个字节的文字
+            $str .= '\u'.base_convert(ord($c), 10, 16).base_convert(ord($c2), 10, 16);
+        }
+        else
+        {
+            $str .= $c2;
+        }
+    }
+    return $str;
+}
+
+// 将UNICODE编码后的内容进行解码，编码后的内容格式：\u56fe\u7247 （原始：图片）
+function unicode_decode($name)
+{
+    // 转换编码，将Unicode编码转换成可以浏览的utf-8编码
+    $pattern = '/([\w]+)|(\\\u([\w]{4}))/i';
+    preg_match_all($pattern, $name, $matches);
+    if (!empty($matches))
+    {
+        $name = '';
+        for ($j = 0; $j < count($matches[0]); $j++)
+        {
+            $str = $matches[0][$j];
+            if (strpos($str, '\\u') === 0)
+            {
+                $code = base_convert(substr($str, 2, 2), 16, 10);
+                $code2 = base_convert(substr($str, 4), 16, 10);
+                $c = chr($code).chr($code2);
+                $c = iconv('UCS-2', 'UTF-8', $c);
+                $name .= $c;
+            }
+            else
+            {
+                $name .= $str;
+            }
+        }
+    }
+    return $name;
+}
--- application/route.php
+++ application/route.php
@@ -1,4 +1,18 @@
 <?php
 use think\Route;
 
-Route::get('api/:version/hello', 'api/:version.Score/hello');
+// Sheet
+Route::get('api/:version/sheet/:id', 'api/:version.Sheet/getSheet',[], ['id'=>'\d+']);
+Route::post('api/:version/sheet', 'api/:version.Sheet/createSheet');
+
+// Work
+Route::get('api/:version/work/:id', 'api/:version.Work/getWork', [], ['id'=>'\d+']);
+Route::post('api/:version/work', 'api/:version.Work/createWork');
+
+// User
+Route::get('api/:version/user', 'api/:version.User/getUser');
+Route::post('api/:version/user', 'api/:version.User/addOrUpdateUser');
+
+// Token
+Route::post('api/:version/token/user', 'api/:version.Token/getToken');
+Route::post('api/:version/token/verify', 'api/:version.Token/verifyToken');
